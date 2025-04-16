3a13ccc4c5ae19f9851a743d8ae8f403edd398e6
estiven2111 <estiven.a.martinez@gmail.com>
2025-04-16 18:48:05 -0500

    urbanclub

--- src/env.js
+++ src/env.js
@@ -2,7 +2,9 @@
 // URL_BACK = "http://localhost:3001"
 
 URL_FRONT = "https://www.urbanclub.club" 
-URL_BACK =  "https://backeste-production.up.railway.app"
+// URL_BACK =  "https://backeste-production.up.railway.app"
+URL_BACK =  "https://urbanclub-backend.onrender.com/"
+
 
 // URL_FRONT = "https://urbanclub.vercel.app" 
 // URL_FRONT = "https://tecnoeam.com/urban" 
